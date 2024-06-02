e184f983abd825edfa028adf3570b7a223b9ce15
vinithmh <vinithmh2000@gmail.com>
2024-06-03 00:16:56 +0530

    dwdfcew

--- backend.tf
+++ backend.tf
@@ -1,10 +1,10 @@
 terraform {
   backend "s3" {
-    bucket         = "s3-revhire-backend-user-remote"
+    bucket         = "s3-revhire-backend-user-remote-new"
     key            = "terraform.tfstate"
     region         = "us-east-1"
     encrypt        = true
-    dynamodb_table = "revhire-user-backend-dynamodb"
+    dynamodb_table = "revhire-user-backend-dynamodb-new"
   }
 }
 
