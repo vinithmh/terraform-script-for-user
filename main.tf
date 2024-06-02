162b03289240b25ee82193ceb5800a98b886cad7
vinithmh <vinithmh2000@gmail.com>
2024-06-03 00:47:20 +0530

    cdsghv

--- main.tf
+++ main.tf
@@ -131,7 +131,7 @@ resource "aws_iam_role_policy_attachment" "codebuild_ssm_full_access" {
 }
 
 resource "aws_codebuild_project" "revhire-user-build" {
-  name          = "revhire-user-build"
+  name          = "revhire-user-build-auto"
   description   = "Build project for revhire-user application"
   service_role  = aws_iam_role.codebuild_role.arn
 
@@ -236,7 +236,7 @@ resource "aws_iam_role_policy_attachment" "codepipeline_codebuild_developeracces
 }
 
 resource "aws_s3_bucket" "codepipeline_bucket" {
-  bucket = "revhire-user-codepipeline-artifacts"
+  bucket = "revhire-user-codepipeline-artifacts-auto"
   force_destroy = true
 }
 
